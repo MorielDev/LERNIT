b5dbe28b0d3c29c9dac3d2f9ee94ec85554ba3fa
ftgeorge <ftgeorgean@gmail.com>
2025-02-24 00:02:14 +0100

    testimonials completed

--- app/pages/testimonials.tsx
+++ app/pages/testimonials.tsx
@@ -12,8 +12,8 @@ import { testimonials } from "../data/arrays";
 
 export default function Testimonials() {
     return (
-        <section className="bg-backgroundColor px-8 flex flex-col justify-evenly items-center h-screen pt-8 pb-16">
-            <HeaderText placeholder="🗣️ From Procrastinators to Earners"/>
+        <section className="bg-backgroundColor px-4 md:px-8 lg:px-8 xl:px-8 2xl:px-8 flex flex-col justify-evenly items-center h-screen pt-8 pb-16">
+            <HeaderText placeholder="🗣️ From Procrastinators to Earners" className="text-center" />
 
             <Swiper
                 modules={[Pagination, Autoplay]}
@@ -43,8 +43,8 @@ export default function Testimonials() {
                             </div>
                         ) : ( */}
                         <div className="w-full flex flex-col justify-center items-center">
-                            <div className="flex flex-col items-center justify-center shadow-xl rounded-2xl p-6 px-14 w-3/6 h-96 bg-white mb-12">
-                                <p className="text-lg italic text-darkGray text-center mb-4">"{item.quote}"</p>
+                            <div className="flex flex-col items-center justify-center shadow-xl rounded-2xl p-6 px-14 w-6/6 md:w-3/6 lg:w-3/6 xl:w-3/6 h-96 md:h-80 lg:h-80 xl:h-96 2xl:h-96 bg-white mb-12">
+                                <p className="text-sm md:text-base lg:text-sm xl:text-lg 2xl:text-lg italic text-darkGray text-center mb-4">"{item.quote}"</p>
                                 <div className="w-16 h-16 rounded-full">
                                     <img
                                         src={item.image}
