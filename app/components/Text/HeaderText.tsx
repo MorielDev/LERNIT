bf38b73914426a8c01a7704ba4400078b66d9bb7
ftgeorge <ftgeorgean@gmail.com>
2025-02-23 23:44:55 +0100

    rewards page responsiveness

--- app/components/Text/HeaderText.tsx
+++ app/components/Text/HeaderText.tsx
@@ -1,11 +1,12 @@
 interface HeaderTextProps {
     placeholder: string;
+    className?: string;
 }
 
-export default function HeaderText({ placeholder }: HeaderTextProps) {
+export default function HeaderText({ placeholder, className }: HeaderTextProps) {
     return (
         <>
-            <h2 className="text-xl md:text-xl lg:text-2xl xl:text-3xl 2xl:text-4xl font-bold text-left md:text-left lg:text-left xl:text-left 2xl:text-left text-darkGray">
+            <h2 className={`text-xl md:text-xl lg:text-2xl xl:text-3xl 2xl:text-4xl font-bold text-left md:text-left lg:text-left xl:text-left 2xl:text-left text-darkGray ${className}`}>
                 {placeholder}
             </h2>
         </>
--- app/pages/rewards.tsx
+++ app/pages/rewards.tsx
@@ -7,28 +7,29 @@ import { MdOutlineArrowForward } from "react-icons/md";
 import HeaderText from "../components/Text/HeaderText";
 import { rewards } from "../data/arrays";
 
-
 export default function Rewards() {
     const [activeTab, setActiveTab] = useState(0);
 
     return (
-        <section className="bg-backgroundColor px-6 h-screen w-full pt-8 pb-16">
-            <HeaderText placeholder="💰 What Can You Earn?" />
+        <section className="bg-backgroundColor px-4 h-screen w-full  pt-8 md:pt-8 lg:pt-0 xl:pt-0 2xl:pt-8 pb-16">
+            <div className="w-full flex flex-col sm:justify-none sm:items-start md:items-center md:justify-center lg:items-center lg:justify-center xl:items-center xl:justify-center">
+                <HeaderText placeholder="💰 What Can You Earn?" className="text-center" />
 
-            {/* Tab Navigation */}
-            <div className="flex justify-center gap-8 mb-10">
-                {rewards.map((reward, index) => (
-                    <button
-                        key={index}
-                        className={`px-4 py-2 rounded-lg transition ${activeTab === index
-                            ? "bg-primary text-white"
-                            : "bg-transparent text-primary"
-                            }`}
-                        onClick={() => setActiveTab(index)}
-                    >
-                        {reward.title}
-                    </button>
-                ))}
+                {/* Tab Navigation */}
+                <div className="flex overflow-x-auto whitespace-nowrap gap-4 md:gap-8 lg:gap-8 xl:gap-8 mb-10 md:mb-10 lg:mb-0 xl:mb-10 2xl:mb-10 mt-4 scrollbar-hide overscroll-none touch-pan-x">
+                    {rewards.map((reward, index) => (
+                        <button
+                            key={index}
+                            className={`px-4 md:px-4 lg:px-2 xl:px-4 2xl:px-4 py-2 rounded-lg transition text-sm md:text-sm lg:text-sm xl:text-base 2xl:text-base  ${activeTab === index
+                                ? "bg-primary text-white"
+                                : "bg-transparent text-primary"
+                                }`}
+                            onClick={() => setActiveTab(index)}
+                        >
+                            {reward.title}
+                        </button>
+                    ))}
+                </div>
             </div>
 
             {/* Active Tab Content */}
@@ -40,16 +41,16 @@ export default function Rewards() {
                 transition={{ duration: 0.5 }}
                 className="text-center max-w-4xl mx-auto"
             >
-                <div className="w-12/12 h-[36rem]">
+                <div className="w-12/12 h-[22rem] md:h-[22rem] lg:h-[22rem] xl:h-[28rem] 2xl:h-[36rem]">
                     <Lottie animationData={rewards[activeTab].animations} loop={true} style={{ width: '100%', height: '100%' }} />
                 </div>
-                <p className="text-lg">{rewards[activeTab].description}</p>
+                <p className="text-lg md:text-base lg:text-base xl:text-lg 2xl:text-lg">{rewards[activeTab].description}</p>
             </motion.div>
 
             {/* Call to Action */}
-            <div className="w-full flex justify-center items-center mt-10">
+            <div className="w-full flex justify-center items-center mt-10 lg:mt-3 xl:mt-10 2xl:mt-10">
                 <SecondaryButton primary={true} placeholder="Start Earning Rewards">
-                    <MdOutlineArrowForward className="text-xl text-white" />
+                    <MdOutlineArrowForward className="text-base md:text-base lg:text-base xl:text-xl 2xl:text-xl text-white" />
                 </SecondaryButton>
             </div>
         </section>
